fab5972efc3dc8fecbd5830eb3425b2b5c4c6ad7
Yue Hang <euhanglee@gmail.com>
2025-09-19 04:01:41 +0800

    google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MLF7GS89');`
+            })(window,document,'script','dataLayer','GTM-5FGDBWK3');`
           }}
         />
         {/* End Google Tag Manager */}
